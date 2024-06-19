f6158c4d7cb76c80b1f2c8e9f91c9a9cb48959be
Aram <aramsaribekyan1@gmail.com>
2024-06-19 08:09:55 +0400

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Aram on 19.06.24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
